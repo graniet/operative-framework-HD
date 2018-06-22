52c26bebc0a2cda445d3169d06453cdc58d60896
Tristan Granier <graniet75@gmail.com>
2018-06-22 11:18:36 +0200

    bug: resolve forgetting of 127.0.0.1 in login

--- framework/app.py
+++ framework/app.py
@@ -176,4 +176,4 @@ def select_project_task_unique():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=config.BACKEND_PORT)
+    app.run(debug=True, host='0.0.0.0', port=config.BACKEND_PORT)
